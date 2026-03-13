82aeded4719110edfd9b7161de013875ab6ef992
robertords1963 <roberto_rodrigues@msn.com>
2026-03-13 16:52:41 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -338,7 +338,7 @@ const handleEmployeeLogin = async () => {
   localStorage.setItem('employeeLoggedIn', 'true');
   localStorage.setItem('employeeId', employeeId);
   setEmployeePassword('');
-  window.scrollTo(0, 0);
+  setTimeout(() => window.scrollTo(0, 0), 500);
   } catch (error) {
     console.error('Login error:', error);
     setLoginError('Login failed. Please try again.');
